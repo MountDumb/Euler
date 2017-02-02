cbe6ee4b4ac8d88fe026a6b2f4bc4edd2daef21c
UrbanSkurk <UrbanSkurk@VARMEVRK>
2017-02-02 11:42:18 +0100

    Add project files.

---
+++ ProjectEuler/Eulers.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Eulers
+    {
+        public Eulers()
+        {
+
+        }
+
+        #region Problem 1
+        public int MultipleThreeFive()
+        {
+            int output = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    output += i;
+                }
+            }
+
+            return output;
+        }
+        #endregion
+
+        #region Problem 2 - Fibonacci
+
+        public int Fibbonaci()
+        {
+            int output = 0;
+            int tempA = 0;
+            int tempB = 1;
+            int tempC = 0;
+
+            while (tempB <= 4000000)
+            {
+                tempC = tempA + tempB;
+                tempA = tempB;
+                tempB = tempC;
+
+                if (tempB % 2 == 0)
+                {
+                    output += tempC;
+                }
+                
+            }
+            return output;
+        }
+        #endregion
+
+
+    }
+
+
+}
---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        Eulers eu = new Eulers();
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.MultipleThreeFive();
+            p.Fibonacci();
+
+            Console.ReadKey();
+            
+        }
+
+        public void MultipleThreeFive()
+        {
+            Console.WriteLine(eu.MultipleThreeFive());
+        }
+
+        public void Fibonacci()
+        {
+            Console.WriteLine(eu.Fibbonaci());
+        }
+    }
+}
